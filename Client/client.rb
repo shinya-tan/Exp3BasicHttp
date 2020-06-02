c4aa50e129d6c0beab1f99569dfb58bbe2d13ffa
shinya yoshitaka <ushimiyay20@gmail.com>
2020-06-03 04:16:48 +0900

    add api link hi

--- Client/client.rb
+++ Client/client.rb
@@ -1,19 +1,9 @@
 require 'httpclient'
 
-base_url = 'http://127.0.0.1:2000'
+base_url = 'http://127.0.0.1:8080/hi'
 
 client = HTTPClient.new()
 
 response = client.get(base_url)
 puts response.status #=> 200
 puts response.body #=> {}
-
-# client.post(base_url, {text: 'todo'}, 'Content-Type' => 'application/json')
-
-# response = client.get(base_url)
-# puts response.status #=> 200
-# puts response.body #=> {"todos": [{"id": 1, "text": "todo"}]}
-
-# response = client.get(base_url + '/1')
-# puts response.status #=> 200
-# puts response.body #=> {"id": 1, "text": "todo"}
--- Server/server.rb
+++ Server/server.rb
@@ -1,18 +1,16 @@
 require 'socket'
 
-server = TCPServer.new 2000
+server = TCPServer.new 8080
 loop do
   client = server.accept
-  headers = []
-  while header = client.gets
-    break if header.chomp.empty?
-    headers << header.chomp
+  request = client.gets
+  if(request.split(' ')[1] == "/hi") then
+    ip_address = Socket.getifaddrs.select{|x| x.name == "eth0" and x.addr.ipv4?}.first.addr.ip_address
+    client.puts "HTTP/1.0 200 OK"
+    client.puts "Content-Type: text/plain"
+    client.puts "Hello: BasicHTTP!"
+    client.puts
+    client.puts "#{ip_address}"
   end
-  # p headers
-  ip_address = Socket.getifaddrs.select{|x| x.name == "en0" and x.addr.ipv4?}.first.addr.ip_address
-  client.puts "HTTP/1.0 200 OK"
-  client.puts "Content-Type: text/plain"
-  client.puts
-  client.puts "#{ip_address}"
-  client.close
+    client.close
 end
