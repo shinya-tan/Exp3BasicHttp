f99ad5b2412056b47ff1a23cd498bb181652c931
shinya yoshitaka <ushimiyay20@gmail.com>
2020-06-03 16:39:41 +0900

    change HTTP/1.0 to HTTP/1.1

--- Server/server.rb
+++ Server/server.rb
@@ -6,7 +6,7 @@ loop do
   request = client.gets
   if(request.split(' ')[1] == "/hi") then
     ip_address = Socket.getifaddrs.select{|x| x.name == "eth0" and x.addr.ipv4?}.first.addr.ip_address
-    client.puts "HTTP/1.0 200 OK"
+    client.puts "HTTP/1.1 200 OK"
     client.puts "Content-Type: text/plain"
     client.puts "Hello: BasicHTTP!"
     client.puts
